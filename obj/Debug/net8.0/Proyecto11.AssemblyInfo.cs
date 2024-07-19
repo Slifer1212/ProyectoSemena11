a679613eb912f2e17f27fae0e51614c21dc93473
Yunior <Supremeyunior008@gmailcom>
2024-07-19 18:36:51 -0400

    GitIgnore

--- obj/Debug/net8.0/Proyecto11.AssemblyInfo.cs
+++ obj/Debug/net8.0/Proyecto11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b0b5cb76b290c6fda46a762d5a422927586da59")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
